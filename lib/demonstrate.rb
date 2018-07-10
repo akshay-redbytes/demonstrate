c401d452f5eae905c13512acf2359e0e49e7ba92
Akshay Shirole <akshay@redbytes.in>
2018-07-10 12:50:33 +0530

    The beginnings of the foodie gem

---
+++ lib/demonstrate.rb
@@ -0,0 +1,10 @@
+require "demonstrate/version"
+
+module Demonstrate
+  # Your code goes here...
+  def self.dummy
+    p "====================================="
+    p "BRO IT WORKED !!!!!!!!!!!!!!!!!!!!!!!"
+    p "*************************************"
+  end
+end
